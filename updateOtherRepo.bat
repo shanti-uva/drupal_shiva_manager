4cc1335e02ced0f6a7918edf749430c1d4abfb6c
Than Grove <than@virginia.edu>
2012-10-22 17:00:16 -0400

    removing unnecessary php file

--- updateOtherRepo.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-SET /P msg=Enter the update message for this change: 
-svn commit -m"%msg%"
-C:
-cd C:\wamp\www\drupal7\sites\all\modules\shivanode
-svn update
-
-echo Done!
-set /p msg=Press Enter to Close!
